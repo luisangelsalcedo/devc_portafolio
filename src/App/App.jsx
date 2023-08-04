181eb4704fb04280e2a3374fd38a0a771a9556ed
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-04 15:05:35 -0500

    PersonalDetails/Add mock data

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,23 @@
 import { PersonalDetails } from '../components';
 
+const person = {
+  name: 'Billy Pearson',
+  occupation: 'Front-end developer',
+  email: 'billy@example.com',
+  phone: '(+603) 546 624 342',
+  image: 'billy-pearson.png',
+  description: [
+    <p key='p-1'>
+      Self-motivated developer, who is willing to learn and create outstanding
+      UI applications.
+    </p>,
+    <p key='p-2'>
+      Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque
+      feugiat malesuada molestie.
+    </p>,
+  ],
+};
+
 export function App() {
   return (
     <>
