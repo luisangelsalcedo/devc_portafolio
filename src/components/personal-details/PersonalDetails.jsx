cd8efcb9cb3e346a6cc147deebea1a975825b487
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-04 15:11:08 -0500

    PersonalDetails/Add html structure to component

--- src/components/personal-details/PersonalDetails.jsx
+++ src/components/personal-details/PersonalDetails.jsx
@@ -2,7 +2,26 @@ import PropTypes from 'prop-types';
 import './PersonalDetails.scss';
 
 export function PersonalDetails({ data }) {
-  return 'PersonalDetails';
+  return (
+    <section className='personal-details'>
+      <div className='panel'>
+        <div className='panel__image'>IMAGE</div>
+        <div className='panel__content'>
+          <div className='panel__content-header'>
+            <div>
+              <h1>{data.name}</h1>
+              <span>{data.occupation}</span>
+            </div>
+            <div>
+              <div className='email icon'>{data.email}</div>
+              <div className='phone icon'>{data.phone}</div>
+            </div>
+          </div>
+          <div className='panel__content-body'>{data.description}</div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 PersonalDetails.propTypes = {
