c502ea656e1d88905b7f0ade7e387164a97d8725
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-10 06:54:28 -0500

    Skills: Add html structure of component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,11 @@
-import { PersonalDetails } from '../components';
+import { PersonalDetails, Skills } from '../components';
 import { DIRECTION_COLUMN } from '../helpers/constants';
 import { person } from '../mocks';
 
 export function App() {
   return (
     <>
+      <Skills />
       <PersonalDetails data={person} />
       <PersonalDetails data={person} direction={DIRECTION_COLUMN} />
     </>
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,5 +1,35 @@
+import PropTypes from 'prop-types';
+import { Panel } from '../elements';
+import { DIRECTION_COLUMN, DIRECTION_ROW } from '../../helpers/constants';
 import './Skills.scss';
 
-export function Skills() {
-  return 'Skills component';
+export function Skills({ direction }) {
+  return (
+    <section className='skills'>
+      <Panel direction={direction}>
+        <Panel.Header>FRONT END</Panel.Header>
+        <Panel.Body>
+          <div className='skills__list'>
+            <div className='skills__list-item'>
+              <div className='skills__list-item-label'>LABEL</div>
+              <div className='skills__list-item-range'>
+                <div style={{ width: '100%' }}></div>
+              </div>
+              <div className='skills__list-item-label'>LABEL</div>
+              <div className='skills__list-item-range'>
+                <div style={{ width: '100%' }}></div>
+              </div>
+              <div className='skills__list-item-label'>LABEL</div>
+              <div className='skills__list-item-range'>
+                <div style={{ width: '100%' }}></div>
+              </div>
+            </div>
+          </div>
+        </Panel.Body>
+      </Panel>
+    </section>
+  );
 }
+Skills.propTypes = {
+  direction: PropTypes.oneOf([DIRECTION_ROW, DIRECTION_COLUMN]),
+};
