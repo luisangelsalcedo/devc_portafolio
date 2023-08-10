0b8a9560d7dc5131130b56d1bf7216abea9b5b9c
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-10 07:53:55 -0500

    Skills: Add SkillItem component

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,6 +3,27 @@ import { Panel } from '../elements';
 import { DIRECTION_COLUMN, DIRECTION_ROW } from '../../helpers/constants';
 import './Skills.scss';
 
+function SkillItem({ children, range }) {
+  console.log(range);
+  return (
+    <div className='skills__list-item'>
+      <div className='skills__list-item-label'>{children}</div>
+      <div className='skills__list-item-range'>
+        <div style={{ width: `${range}%` }}></div>
+      </div>
+    </div>
+  );
+}
+
+SkillItem.propTypes = {
+  children: PropTypes.node,
+  range: PropTypes.number,
+};
+
+SkillItem.defaultProps = {
+  range: 0,
+};
+
 export function Skills({ direction }) {
   return (
     <section className='skills'>
@@ -10,30 +31,10 @@ export function Skills({ direction }) {
         <Panel.Header>FRONT END</Panel.Header>
         <Panel.Body>
           <div className='skills__list'>
-            <div className='skills__list-item'>
-              <div className='skills__list-item-label'>LABEL</div>
-              <div className='skills__list-item-range'>
-                <div style={{ width: '100%' }}></div>
-              </div>
-            </div>
-            <div className='skills__list-item'>
-              <div className='skills__list-item-label'>LABEL</div>
-              <div className='skills__list-item-range'>
-                <div style={{ width: '100%' }}></div>
-              </div>
-            </div>
-            <div className='skills__list-item'>
-              <div className='skills__list-item-label'>LABEL</div>
-              <div className='skills__list-item-range'>
-                <div style={{ width: '100%' }}></div>
-              </div>
-            </div>
-            <div className='skills__list-item'>
-              <div className='skills__list-item-label'>LABEL</div>
-              <div className='skills__list-item-range'>
-                <div style={{ width: '100%' }}></div>
-              </div>
-            </div>
+            <Skills.Item range={100}>LABEL</Skills.Item>
+            <Skills.Item range={100}>LABEL</Skills.Item>
+            <Skills.Item range={100}>LABEL</Skills.Item>
+            <Skills.Item range={100}>LABEL</Skills.Item>
           </div>
         </Panel.Body>
       </Panel>
@@ -43,3 +44,5 @@ export function Skills({ direction }) {
 Skills.propTypes = {
   direction: PropTypes.oneOf([DIRECTION_ROW, DIRECTION_COLUMN]),
 };
+
+Skills.Item = SkillItem;
